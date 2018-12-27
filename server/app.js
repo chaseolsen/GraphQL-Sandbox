7dd77275589cc226da5e457d6a62529c3002149f
chaseolsen <chase.olsen22@gmail.com>
2018-12-26 21:07:05 -0700

    Finished front to back end project with graphql

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// importing GraphQL schema from schema/schema.js
+const schema = require('./schema/schema');
+
+const app = express();
+
+// allow cross-origin requests
+app.use(cors());
+
+
+// connect to mlab database
+
+// to prevent bots from crawling github and stealing PWs and having to create a .gitignore file, manually copy this to string below...
+
+// User: chase
+// PW: test123
+mongoose.connect('mongodb://<user>:<password>123@ds119273.mlab.com:19273/gql-sandbox', { useNewUrlParser: true });
+
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
+
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('Listening for requests on port 4000');
+});
---
+++ server/models/book.js
@@ -0,0 +1,12 @@
+// Book Schema for mLab DB
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = Schema({
+  name: String,
+  genre: String,
+  authorId: String
+});
+
+module.exports = mongoose.model('Book', bookSchema);
